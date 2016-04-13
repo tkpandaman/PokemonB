cd630fe7455abfd3f596f6243f5b42a69cbbf020
CogentInvalid <pchipman@email.arizona.edu>
2016-04-12 22:41:51 -0700

    Added walking/winning

--- src/controller/GameGUI.java
+++ src/controller/GameGUI.java
@@ -1,11 +1,17 @@
 package controller;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 
+import javafx.scene.input.KeyCode;
 import model.Game;
 import view.MapView;
 
 public class GameGUI extends JFrame {
+	
+	private Game game;
 	
 	public static void main(String[] args){
 		GameGUI gui = new GameGUI();
@@ -17,13 +23,39 @@ public class GameGUI extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(1024, 768);
 		
-		Game game = new Game();
+		game = new Game();
 		
 		MapView mapView = new MapView(game);
 		
 		game.addObserver(mapView);
 		
 		this.add(mapView);
+		
+		this.addKeyListener(new ArrowKeyListener());
+		
+	}
+	
+	private class ArrowKeyListener implements KeyListener{
+
+		@Override
+		public void keyPressed(KeyEvent event) {
+			if (event.getKeyCode() == KeyEvent.VK_UP)
+				game.moveUp();
+			if (event.getKeyCode() == KeyEvent.VK_DOWN)
+				game.moveDown();
+			if (event.getKeyCode() == KeyEvent.VK_LEFT)
+				game.moveLeft();
+			if (event.getKeyCode() == KeyEvent.VK_RIGHT)
+				game.moveRight();
+		}
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+		}
+
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+		}
 		
 	}
 	
--- src/model/Game.java
+++ src/model/Game.java
@@ -8,8 +8,12 @@ import java.util.Observable;
 public class Game extends Observable {
 	
 	private Map map;
+	private Trainer trainer;
 	private int playerX = 0;
 	private int playerY = 0;
+	private int steps = 0;
+	
+	private boolean won = false;
 	
 	public Game(){
 		
@@ -34,6 +38,65 @@ public class Game extends Observable {
 	// Returns the current map.
 	public Map getMap(){
 		return map;
+	}
+	
+	public void takeStep(int x, int y){
+		if (!won){
+			playerX += x;
+			playerY += y;
+			steps++;
+			if (steps >= 500) won = true;
+			setChanged();
+			notifyObservers();
+		}
+	}
+	
+	public int getSteps(){
+		return steps;
+	}
+	
+	public void moveLeft(){
+		if (playerX > 0){
+			if (!map.tileAt(playerX-1, playerY).isSolid()){
+				takeStep(-1, 0);
+			}
+		}
+	}
+	
+	public void moveRight(){
+		if (playerX < map.getWidth()-1){
+			if (!map.tileAt(playerX+1, playerY).isSolid()){
+				takeStep(1, 0);
+			}
+		}
+	}
+	
+	public void moveUp(){
+		if (playerY > 0){
+			if (!map.tileAt(playerX, playerY-1).isSolid()){
+				takeStep(0, -1);
+			}
+		}
+	}
+	
+	public void moveDown(){
+		if (playerY < map.getHeight()-1){
+			if (!map.tileAt(playerX, playerY+1).isSolid()){
+				takeStep(0, 1);
+			}
+		}
+	}
+	
+	public int getPlayerX(){
+		return playerX;
+	}
+	
+	public int getPlayerY(){
+		return playerY;
+	}
+	
+	public boolean gameWon(){
+		return won;
 	}
 	
 }
