feb9a2397e234c2529bb9c0f850a281ac65aabe6
Alex Katzfey <akatzfey@yahoo.com>
2016-04-15 15:18:57 -0700

    Fixed saving and Unified Gamestate and Game class. Removed some tests.

--- src/model/Game.java
+++ src/model/Game.java
@@ -3,67 +3,49 @@ package model;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.io.Serializable;
 import java.util.Observable;
 
-public class Game extends Observable {
-	
+public class Game extends Observable implements Serializable {
+
 	private Map map;
 	private Trainer trainer = new Trainer("Sir Dumplestein");
 	private int playerX = 0;
 	private int playerY = 0;
-	
+
 	private State state = State.NORMAL;
-	
-	public Game(){
-		
+
+	public Game(Map map){
+
 		//Load map
-		File mapFile = new File("levels/emerald-test");
-		try{
-			FileInputStream fileIn = new FileInputStream(mapFile);
-			ObjectInputStream in = new ObjectInputStream(fileIn);
-			
-			//read objects from saved data
-			map = (Map) in.readObject();
-			
-			in.close();
-			fileIn.close();
-			setChanged();
-			notifyObservers();
-		} catch(Exception ee){
-		}
-		
-		Gamestate state = Gamestate.getInstance();
-		this.addObserver(state);
-		
+		this.map = map;
+		update();
+
 	}
 	
-	public void loadState(){
-		Gamestate gamestate = Gamestate.getInstance();
-		map = gamestate.getCurrentMap();
-		trainer = gamestate.getTrainer();
-		playerX = gamestate.getTrainerX();
-		playerY = gamestate.getTrainerY();
-		state = gamestate.getState();
+	//call to update observers
+	public void update(){
 		setChanged();
 		notifyObservers();
 	}
-	
+
+
 	// Returns the current map.
 	public Map getMap(){
 		return map;
 	}
-	
+
 	public void takeStep(int x, int y){
 		if (state == State.NORMAL){
 			playerX += x;
 			playerY += y;
 			trainer.takeStep();
 			if (trainer.getStepsLeft() <= 0) state = State.WIN;
-			setChanged();
-			notifyObservers();
+			update();
 		}
 	}
 	
+
 	public void moveLeft(){
 		if (playerX > 0){
 			if (!map.tileAt(playerX-1, playerY).isSolid()){
@@ -71,7 +53,7 @@ public class Game extends Observable {
 			}
 		}
 	}
-	
+
 	public void moveRight(){
 		if (playerX < map.getWidth()-1){
 			if (!map.tileAt(playerX+1, playerY).isSolid()){
@@ -79,7 +61,7 @@ public class Game extends Observable {
 			}
 		}
 	}
-	
+
 	public void moveUp(){
 		if (playerY > 0){
 			if (!map.tileAt(playerX, playerY-1).isSolid()){
@@ -87,7 +69,7 @@ public class Game extends Observable {
 			}
 		}
 	}
-	
+
 	public void moveDown(){
 		if (playerY < map.getHeight()-1){
 			if (!map.tileAt(playerX, playerY+1).isSolid()){
@@ -95,21 +77,21 @@ public class Game extends Observable {
 			}
 		}
 	}
-	
+
 	public int getPlayerX(){
 		return playerX;
 	}
-	
+
 	public int getPlayerY(){
 		return playerY;
 	}
-	
+
 	public Trainer getTrainer(){
 		return trainer;
 	}
-	
+
 	public State getState(){
 		return state;
 	}
-	
+
 }
