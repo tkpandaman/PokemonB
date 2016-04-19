2c654f30cf02c3108e35a8580137b191b8910f3d
Alex Katzfey <akatzfey@yahoo.com>
2016-04-19 13:21:51 -0700

    Added to Battle class and created BattleTest class which covers most of the code

--- src/model/Battle.java
+++ src/model/Battle.java
@@ -1,52 +1,149 @@
 package model;
 
+import java.util.Random;
+
 import model.pokemon.Pokemon;
 
+/**
+ * The Class Battle.
+ * @author AlexKatzfey
+ */
 public class Battle {
 	
 	private double chanceOfRun;
 	private double chanceOfCapture;
 	private int turnsTillFlee;
-	private int turn;
+	private int turn; 
+	private boolean willFlee;
 	private Pokemon poke;
 	private Trainer t;
+	private Random rand;
 	
-	private static final int ROCK_DAMAGE = 10;
-	private static final double BAIT_CHANCE = 0.05;
+	//Change these to affect the balance
+	public static final int ROCK_DAMAGE = 20;
+	public static final double BAIT_CHANCE = 0.05;
+	public static final double MIN_RUN_CHANCE = 0.10;
+	public static final double MIN_CAPTURE_CHANCE = 0.35;
 	
+	/**
+	 * Instantiates a new battle.
+	 *
+	 * @param t the trainer
+	 * @param p the pokemon
+	 */
 	public Battle(Trainer t, Pokemon p){
 		this.poke = p;
 		this.t = t;
 		this.turnsTillFlee = p.getTurnsTillFlee();
 		this.turn = 1;
 		this.chanceOfRun = poke.getLikelyRun() / 100;
-		setCaptureChance();
+		this.willFlee = false;
+		this.rand = new Random();
+		this.chanceOfCapture = MIN_CAPTURE_CHANCE;
+	}
+	
+	/**
+	 * Instantiates a new battle with random seed for testability.
+	 *
+	 * @param t the trainer
+	 * @param p the pokemon
+	 * @param r the random
+	 */
+	public Battle(Trainer t, Pokemon p, Random r){
+		this.poke = p;
+		this.t = t;
+		this.turnsTillFlee = p.getTurnsTillFlee();
+		this.turn = 1;
+		this.chanceOfRun = (double)poke.getLikelyRun() / 100;
+		this.willFlee = false;
+		this.rand = r;
+		this.chanceOfCapture = MIN_CAPTURE_CHANCE;
 	}
 	
 	private void setCaptureChance(){
-		this.chanceOfCapture = 1 - (poke.getCurHP() / poke.getMaxHP());
+		this.chanceOfCapture = 1.0 - ((double)poke.getCurHP() / poke.getMaxHP()) + MIN_CAPTURE_CHANCE;
+	}
+	
+	/**
+	 * Gets the capture chance.
+	 *
+	 * @return the capture chance
+	 */
+	public double getCaptureChance(){
+		return this.chanceOfCapture;
+	}
+	
+	/**
+	 * Gets the flee chance.
+	 *
+	 * @return the flee chance
+	 */
+	public double getFleeChance(){
+		return this.chanceOfRun;
+	}
+	
+	/**
+	 * Gets the turn.
+	 *
+	 * @return the turn
+	 */
+	public int getTurn(){
+		return this.turn;
 	}
 	
 	private void endTurn(){
 		//End of turn actions here
+		this.turn++;
+		
+		if (turn >= turnsTillFlee || rand.nextDouble() < chanceOfRun){
+			this.willFlee = true;
+		}
+		
 	}
 	
+	/**
+	 * Check to see if Pokemon ran away each turn.
+	 *
+	 * @return true, if successful
+	 */
+	public boolean pokemonRanAway(){
+		return this.willFlee;
+	}
+	
+	/**
+	 * Throw rock. Increases chance of capturing pokemon.
+	 */
 	public void throwRock(){
 		this.poke.takeDamage(ROCK_DAMAGE);
 		setCaptureChance();
 		endTurn();
 	}
 	
+	/**
+	 * Throw bait. Lower's chance of pokemon running.
+	 */
 	public void throwBait(){
-		this.chanceOfRun -= BAIT_CHANCE;
+		if(this.chanceOfRun >= MIN_RUN_CHANCE + BAIT_CHANCE) this.chanceOfRun -= BAIT_CHANCE;
 		endTurn();
 	}
 	
-	public void throwSafariBall(){
+	/**
+	 * Throw safari ball. If successful, pokemon is added to Trainer's backpack.
+	 *
+	 * @return true, if the pokemon is captured
+	 */
+	public boolean throwSafariBall(){
 		Pokeball ball = this.t.openPack().usePokeball();
 		if(ball != null){
-			
+			ball.random = this.rand;
+			endTurn();
+			boolean captured = ball.use(this.chanceOfCapture);
+			if (captured) t.openPack().addPokemon(this.poke); //pokemon is added to backpack if captured
+			return captured;
 		}
+		return false; //This should never be called as the player 
+					  //should not be able to throw a pokeball when they have 0 left.
+		
 	}
 	
 	
---
+++ src/tests/battleTest.java
@@ -0,0 +1,88 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import model.Backpack;
+import model.Battle;
+import model.Trainer;
+import model.pokemon.Arbok;
+import model.pokemon.Butterfree;
+import model.pokemon.Charizard;
+import model.pokemon.Pikachu;
+import model.pokemon.Pokemon;
+
+import org.junit.Test;
+
+public class battleTest {
+
+	private Random rand = new Random(1337L);
+	private Trainer ash = new Trainer("Ash");
+
+	@Test
+	public void setupBattleTest() {
+		Pokemon arbok = new Arbok();
+		Battle b = new Battle(ash, arbok, rand);
+		Battle b2 = new Battle(ash, arbok);
+		assertFalse(b.pokemonRanAway());
+		assertFalse(b2.pokemonRanAway());
+	}
+
+	@Test
+	public void throwRockTest() {
+		Pokemon c = new Charizard();
+		Battle b = new Battle(ash, c, rand);
+
+		b.throwRock();
+		assertEquals(c.getMaxHP() - Battle.ROCK_DAMAGE, c.getCurHP());
+		assertFalse(b.pokemonRanAway());
+
+		for(int i = 0; i < 4; i++){ //throw 4 rocks = 4 turns passing 
+			b.throwRock();
+		}
+		assertTrue(b.pokemonRanAway());
+	}
+
+	@Test
+	public void throwBaitTest() {
+		Pokemon c = new Charizard();
+		Battle b = new Battle(ash, c, rand);
+		
+		double runChance = (double)c.getLikelyRun()/100;
+
+		assertEquals(runChance, b.getFleeChance(), 0.00001);
+		b.throwBait();
+		assertEquals(runChance - Battle.BAIT_CHANCE , b.getFleeChance(), 0.00001);
+		assertFalse(b.pokemonRanAway());
+
+		for(int i = 0; i < 10; i++){ //throw bait 10 times to test min run chance
+			b.throwBait();
+		}
+		assertEquals(Battle.MIN_RUN_CHANCE , b.getFleeChance(), 0.00001);
+		assertTrue(b.pokemonRanAway());
+	}
+	
+	@Test
+	public void throwSafariBallTest(){
+		Pokemon c = new Pikachu();
+		Trainer m = new Trainer("Misty");
+		Battle b = new Battle(m, c, rand);
+		
+		assertEquals(Battle.MIN_CAPTURE_CHANCE , b.getCaptureChance(), 0.00001);
+		assertFalse(b.throwSafariBall());
+		assertEquals(Backpack.INI_POKEBALLS - 1, m.openPack().getPokeballsLeft());
+		
+		for(int i = 0; i < 4; i++){ 
+			b.throwBait();
+		}
+		assertEquals(6, b.getTurn());
+		assertFalse(b.pokemonRanAway());
+		assertTrue(b.throwSafariBall());
+		assertEquals(1, m.openPack().getPokemonCaptured());
+	}
+	
+	
+
+
+}
