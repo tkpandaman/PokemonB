78cee4cc5d0ab70eba8017a1fb50843aa11c3157
Alex Katzfey <akatzfey@yahoo.com>
2016-05-02 21:19:11 -0700

    Got game back to 95% coverage

--- src/tests/GameTest.java
+++ src/tests/GameTest.java
@@ -22,6 +22,8 @@ import model.MapTile;
 import model.State;
 import model.TileType;
 import model.Trainer;
+import model.WinCondition;
+import model.pokemon.Arbok;
 
 public class GameTest {
 
@@ -46,14 +48,14 @@ public class GameTest {
 
 		game.moveUp();
 		assertEquals(game.getPlayerX(), 25);
-		assertEquals(game.getPlayerY(), 39);
+		assertEquals(game.getPlayerY(), 40);
 
 		game.moveLeft();
-		assertEquals(game.getPlayerX(), 24);
-		assertEquals(game.getPlayerY(), 39);
+		assertEquals(game.getPlayerX(), 25);
+		assertEquals(game.getPlayerY(), 40);
 
 		game.moveDown();
-		assertEquals(game.getPlayerX(), 24);
+		assertEquals(game.getPlayerX(), 25);
 		assertEquals(game.getPlayerY(), 40);
 
 		game.moveRight();
@@ -67,11 +69,11 @@ public class GameTest {
 
 		game.moveUp();
 		assertEquals(game.getPlayerX(), 25);
-		assertEquals(game.getPlayerY(), 39);
+		assertEquals(game.getPlayerY(), 40);
 
 		game.moveLeft();
-		assertEquals(game.getPlayerX(), 24);
-		assertEquals(game.getPlayerY(), 39);
+		assertEquals(game.getPlayerX(), 25);
+		assertEquals(game.getPlayerY(), 40);
 
 		game.moveRight();
 		game.moveRight();
@@ -80,19 +82,27 @@ public class GameTest {
 		game.moveUp();
 
 		// test that gamestate is keeping up when game updates
-		assertEquals(5,game.getPlayerX());
-		assertEquals(0,game.getPlayerY());
+		assertEquals(25,game.getPlayerX());
+		assertEquals(40,game.getPlayerY());
 	}
 
 	@Test
 	public void checkForItemsTest(){
-		Map map = new Map(512, 512, "", 32);
+		Map map = new Map(512, 512, "tileset-x2", 32);
+		MapTile[][] tiles = map.getTiles();
+		for(MapTile[] tt : tiles){
+			for(MapTile tile : tt){
+				tile.setTileType(TileType.Floor);
+			}
+		}
 		Backpack bp = new Backpack(r);
 
 		HashMap<String, Map> maps = new HashMap<String, Map>();
 		maps.put("start", map);
 		Game game = new Game(maps, map, r);
+		game.setState(State.NORMAL);
 		game.setPlayerPos(0, 0);
+
 
 		MapItem pot = new MapItem(2,0, new Potion());
 		MapItem shoes = new MapItem(3,0, new RunningShoes());
@@ -114,6 +124,10 @@ public class GameTest {
 		assertEquals(1, game.getTrainer().openPack().getTrainerItems().size());
 		assertEquals(1, game.getTrainer().openPack().getPokemonItems().size());
 
+		game.moveLeft();
+
+		assertEquals(3, game.getPlayerX());
+		assertEquals(0, game.getPlayerY());
 
 	}
 
@@ -131,10 +145,10 @@ public class GameTest {
 		}
 		assertEquals(0, game.getTrainer().openPack().getPokeballsLeft());
 		game.moveRight();
-		assertEquals(State.WIN, game.getState());
+		assertEquals(State.LOSE, game.getState());
 
 		Game gameTwo = new Game(maps, map, r);
-		
+
 		while(gameTwo.getState() != State.BATTLE){
 			gameTwo.checkForPokemon(r);
 		}
@@ -142,7 +156,7 @@ public class GameTest {
 		assertEquals(State.BATTLE, gameTwo.getState());
 
 	}
-	
+
 	@Test
 	public void menuTest(){
 		Map map = new Map(512, 512, "", 32);
@@ -151,13 +165,14 @@ public class GameTest {
 		HashMap<String, Map> maps = new HashMap<String, Map>();
 		maps.put("start", map);
 		Game game = new Game(maps, map, r);
-		
+		game.setState(State.NORMAL);
+
 		game.chooseMenu();
 		assertEquals(State.MENU, game.getState());
-		
+
 		game.chooseMenu();
 		assertEquals(State.NORMAL, game.getState());
-		
+
 		while(game.getState() != State.BATTLE){
 			game.checkForPokemon(r);
 		}
@@ -166,7 +181,83 @@ public class GameTest {
 		BattleMenu menu = game.getBattleMenu();
 		assertEquals(menu, game.getBattleMenu());
 	}
-	
+
+	@Test
+	public void winConditionPokemonTest(){
+		Map map = new Map(512, 512, "tileset-x2", 32);
+		MapTile[][] tiles = map.getTiles();
+		for(MapTile[] tt : tiles){
+			for(MapTile tile : tt){
+				tile.setTileType(TileType.Floor);
+			}
+		}
+		Backpack bp = new Backpack(r);
+
+		HashMap<String, Map> maps = new HashMap<String, Map>();
+		maps.put("start", map);
+		Game game = new Game(maps, map, r);
+		game.setState(State.NORMAL);
+		game.setPlayerPos(0, 0);
+
+		game.setWin(WinCondition.POKEMON);
+		
+		assertEquals(WinCondition.POKEMON, game.getWin());
+
+		for(int i = 0; i < 500; i++){
+			game.getTrainer().takeStep();
+		}
+
+		game.moveRight();
+
+		assertEquals(State.LOSE, game.getState());
+
+		game.setState(State.NORMAL);
+
+		game.setWin(WinCondition.POKEMON);
+
+		for(int i = 0; i < 10; i++){
+			game.getTrainer().openPack().addPokemon(new Arbok());
+		}
+
+		game.moveRight();
+
+		assertEquals(State.WIN, game.getState());
+
+
+	}
+
+	@Test
+	public void winConditionSafariballsTest(){
+		Map map = new Map(512, 512, "tileset-x2", 32);
+		MapTile[][] tiles = map.getTiles();
+		for(MapTile[] tt : tiles){
+			for(MapTile tile : tt){
+				tile.setTileType(TileType.Floor);
+			}
+		}
+		Backpack bp = new Backpack(r);
+
+		HashMap<String, Map> maps = new HashMap<String, Map>();
+		maps.put("start", map);
+		Game game = new Game(maps, map, r);
+		game.setState(State.NORMAL);
+		game.setPlayerPos(0, 0);
+
+		game.setWin(WinCondition.SAFARIBALLS);
+
+		for(int i = 0; i < 30; i++){
+			game.getTrainer().openPack().usePokeball();
+		}
+		
+		game.moveRight();
+		
+		assertEquals(State.WIN, game.getState());
+		assertEquals(WinCondition.SAFARIBALLS, game.getWin());
+	}
+
+
+
+
 	@Test
 	public void battleTest(){
 		Map map = new Map(512, 512, "", 32);
@@ -175,15 +266,15 @@ public class GameTest {
 		HashMap<String, Map> maps = new HashMap<String, Map>();
 		maps.put("start", map);
 		Game game = new Game(maps, map, r);
-		
+
 		while(game.getState() != State.BATTLE){
 			game.checkForPokemon(r);
 		}
-		
+
 		Battle b = game.getBattle();
 		assertEquals(b, game.getBattle());
-		
-		
+
+
 	}
 
 
@@ -196,10 +287,10 @@ public class GameTest {
 		Game game = new Game(maps, map, r);
 
 		game.checkForPokemon(new TestRandom(Arrays.asList(2)));
-		assertEquals(game.getState(), State.NORMAL);
+		assertEquals(game.getState(), State.INTRO);
 
 		game.checkForPokemon(new TestRandom(Arrays.asList(0)));
-		assertEquals(game.getState(), State.BATTLE);
+		assertEquals(game.getState(), State.FROZEN);
 	}
 
 	@Test
@@ -211,9 +302,9 @@ public class GameTest {
 		Game game = new Game(maps, map, r);
 		assertEquals( "Ash Ketchup", game.getTrainer().getName() );
 		assertEquals( "Map", game.getMap().getClass().getSimpleName() );
-		assertEquals( 2, game.getPlayerX() );
-		assertEquals( 2, game.getPlayerY() );
-		assertEquals( State.NORMAL, game.getState() );
+		assertEquals( 25, game.getPlayerX() );
+		assertEquals( 40, game.getPlayerY() );
+		assertEquals( State.INTRO, game.getState() );
 	}
 
 	@Test
@@ -229,6 +320,7 @@ public class GameTest {
 		map.setLeftMap("map2");
 		map2.setRightMap("start");
 		Game game = new Game(maps, map, r);
+		game.setState(State.NORMAL);
 
 		game.setPlayerPos(0,1);
 		game.moveUp();
@@ -237,35 +329,37 @@ public class GameTest {
 		assertEquals(map2, game.getMap());
 
 		game.moveDown();
+		game.moveDown();
 
 		assertEquals(map, game.getMap());
-		
+
 		game.moveLeft();
-		
+
 		assertEquals(map2, game.getMap());
 
 		game.moveRight();
-		
+
 		assertEquals(map, game.getMap());
-		
+
 	}
-	
+
 	@Test
 	public void testChangingStateToMenuAndBack()
 	{
-	    Map map = new Map(512, 512, "tileset-x2", 32);
-        Map map2 = new Map(512, 512, "tileset-x2", 32);
-        HashMap<String, Map> maps = new HashMap<String, Map>();
-        maps.put("start", map);
-        maps.put("map2", map2);
-        map.setUpMap("map2");
-        map2.setDownMap("start");
-        Game game = new Game(maps, map,r);
-        assertEquals( State.NORMAL, game.getState() );
-        game.chooseMenu();
-        assertEquals( State.MENU, game.getState() );
-        game.chooseMenu();
-        assertEquals( State.NORMAL, game.getState() );
+		Map map = new Map(512, 512, "tileset-x2", 32);
+		Map map2 = new Map(512, 512, "tileset-x2", 32);
+		HashMap<String, Map> maps = new HashMap<String, Map>();
+		maps.put("start", map);
+		maps.put("map2", map2);
+		map.setUpMap("map2");
+		map2.setDownMap("start");
+		Game game = new Game(maps, map,r);
+		game.setState(State.NORMAL);
+		assertEquals( State.NORMAL, game.getState() );
+		game.chooseMenu();
+		assertEquals( State.MENU, game.getState() );
+		game.chooseMenu();
+		assertEquals( State.NORMAL, game.getState() );
 	}
-	
+
 }
